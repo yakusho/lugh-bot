59d341061d59a9352d4b6125dbf78f05bd46e5f3
Marcos Sebastiao <m.juniorcsebastiao@gmail.com>
2023-10-15 17:10:01 -0300

    Add select for invites after creating meeting room

--- commands/meeting.ts
+++ commands/meeting.ts
@@ -1,16 +1,20 @@
 import {
+  ActionRowBuilder,
   ChannelType,
   ChatInputCommandInteraction,
+  ComponentType,
   Guild,
   SlashCommandBuilder,
-  channelMention
+  UserSelectMenuBuilder,
+  channelMention,
+  userMention
 } from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("meeting")
     .setDescription("Create a private voice channel meeting room")
-    .addStringOption(option => 
+    .addStringOption(option =>
       option
         .setName("name")
         .setDescription("Meeting room channel name")
@@ -28,14 +32,14 @@ export default {
 
     const roomName = interaction.options.getString("name") ?? "Unnamed room";
 
-    const channel = await guild.channels.create({ 
+    const channel = await guild.channels.create({
       name: roomName,
       type: ChannelType.GuildVoice,
       permissionOverwrites: [
         {
           id: interaction.user.id,
           allow: [
-            "ViewChannel", 
+            "ViewChannel",
             "Connect",
             "ManageChannels"
           ]
@@ -49,9 +53,51 @@ export default {
       ]
     });
 
-    await interaction.reply({
-      content: `Meeting room ${channelMention(channel.id)} created succesfully`,
+    const menu = new UserSelectMenuBuilder()
+      .setCustomId("users")
+      .setPlaceholder("Select the users you wish to invite")
+      .setMaxValues(25);
+
+    const row = 
+      new ActionRowBuilder()
+        .addComponents(menu) as ActionRowBuilder<UserSelectMenuBuilder>;
+
+    const message = await interaction.reply({
+      components: [row],
+      content: `You're all set, your meeting room ${channelMention(channel.id)} is prepared!`,
       ephemeral: true
     });
+
+    if (interaction.channel) {
+      const select = await message.awaitMessageComponent({
+        filter: ({ user }) => {
+          return user.id === interaction.user.id;
+        },
+        componentType: ComponentType.UserSelect
+      });
+
+      const users = [];
+      for (const value of select.values) {
+        users.push(await interaction.client.users.fetch(value));
+      }
+
+      for (const user of users) {
+        channel.permissionOverwrites.edit(user.id, {
+          ViewChannel: true,
+          Connect: true
+        });
+
+        user.send({
+          content: `${userMention(interaction.user.id)} has invited you to join them at ${channelMention(channel.id)}`
+        });
+      }
+
+      await message.edit({
+        components: []
+      });
+
+      const names = users.map(({ id }) => userMention(id));
+      select.reply({ content: `You invited ${names.join(", ")}!`, ephemeral: true });
+    }
   }
 };
